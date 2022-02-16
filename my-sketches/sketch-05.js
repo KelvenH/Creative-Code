1c1f4d8731da6599d198d47668710c0b4df565e9
KelvenH <kelvenhughes@me.com>
2022-02-16 18:29:11 +0000

    Created sketch 5 - working with fonts

---
+++ my-sketches/sketch-05.js
@@ -0,0 +1,45 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+let text ='A';
+let fontSize = 1200;
+let fontFamily = 'serif';
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    // context.font= fontSize + 'px ' + fontFamily;
+    context.font= `${fontSize}px ${fontFamily}`;
+    context.textBaseline = 'top';
+    // context.textAlign = 'center';
+
+    
+    const metrics = context.measureText(text);
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+    
+    const x = (width - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
+
+    context.save();
+    context.translate(x, y);
+    
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
+
+    context.fillText(text, 0, 0);
+    
+    context.restore();
+  };
+};
+
+canvasSketch(sketch, settings);
